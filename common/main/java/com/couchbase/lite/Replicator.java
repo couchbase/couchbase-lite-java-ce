f9d9c05b29515f47d2f9cf447b6fd6753e6f2642
G. Blake Meike <blake.meike@couchbase.com>
2020-03-24 15:32:10 -0700

    Abstract Replicator is thread safe New C4 Database lifecycle support (except: CBL-787) Document Id feature (still need tests: CBL-792)

--- common/main/java/com/couchbase/lite/Replicator.java
+++ common/main/java/com/couchbase/lite/Replicator.java
@@ -1,3 +1,20 @@
+//
+// AbstractReplicator.java
+//
+// Copyright (c) 2017 Couchbase, Inc All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 package com.couchbase.lite;
 
 import android.support.annotation.NonNull;
@@ -14,11 +31,8 @@ public final class Replicator extends AbstractReplicator {
     public Replicator(@NonNull ReplicatorConfiguration config) { super(config); }
 
     @Override
-    protected C4Replicator getC4ReplicatorLocked() throws LiteCoreException {
-        final Endpoint target = config.getTarget();
-
-        if (target instanceof URLEndpoint) { return getRemoteC4ReplicatorLocked(((URLEndpoint) target).getURL()); }
-
+    protected C4Replicator createReplicatorForTarget(Endpoint target) throws LiteCoreException {
+        if (target instanceof URLEndpoint) { return getRemoteC4Replicator(((URLEndpoint) target).getURL()); }
         throw new IllegalStateException("unrecognized endpoint type: " + target);
     }
 }
