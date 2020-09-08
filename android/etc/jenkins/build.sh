45eb8c0d91e764d5ded687d3ebd8a25b415d1554
G. Blake Meike <blake.meike@couchbase.com>
2020-09-08 11:31:45 -0700

    pre-branch cleanup

--- android/etc/jenkins/build.sh
+++ android/etc/jenkins/build.sh
@@ -6,7 +6,7 @@
 # These versions must match the versions in lib/build.gradle
 NDK_VERSION='21.3.6528147'
 CMAKE_VERSION='3.10.2.4988404'
-BUILD_TOOLS_VERSION='29.0.3'
+BUILD_TOOLS_VERSION='30.0.2'
 
 MAVEN_URL="http://mobile.maven.couchbase.com/maven2/cimaven"
 
--- android/etc/jenkins/test.sh
+++ android/etc/jenkins/test.sh
@@ -3,26 +3,53 @@
 # Test Couchbase Lite Android, Enterprise Edition
 #
 
+# These versions must match the versions in lib/build.gradle
+NDK_VERSION='21.3.6528147'
+CMAKE_VERSION='3.10.2.4988404'
+BUILD_TOOLS_VERSION='30.0.2'
+
+
 function usage() {
-    echo "Usage: $0 <build number> <reports path>"
+    echo "Usage: $0 <sdk path> <build number> <reports dir>"
     exit 1
 }
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
     usage
 fi
 
-BUILD_NUMBER="$1"
+SDK_HOME="$1"
+if [ -z "$SDK_HOME" ]; then
+    usage
+fi
+
+BUILD_NUMBER="$2"
 if [ -z "$BUILD_NUMBER" ]; then
     usage
 fi
 
-REPORTS="$2"
+REPORTS="$3"
 if [ -z "REPORTS" ]; then
     usage
 fi
 
+SDK_MGR="${SDK_HOME}/tools/bin/sdkmanager"
+
 echo "======== TEST Couchbase Lite Android, Community Edition v`cat ../../version.txt`-${BUILD_NUMBER} on device: ${ANDROID_SERIAL}"
+
+echo "======== Install Toolchain"
+yes | ${SDK_MGR} --licenses > /dev/null 2>&1
+${SDK_MGR} --install "build-tools;${BUILD_TOOLS_VERSION}"
+${SDK_MGR} --install "cmake;${CMAKE_VERSION}"
+${SDK_MGR} --install "ndk;${NDK_VERSION}"
+
+cat <<EOF >> local.properties
+sdk.dir=${SDK_HOME}
+ndk.dir=${SDK_HOME}/ndk/${NDK_VERSION}
+cmake.dir=${SDK_HOME}/cmake/${CMAKE_VERSION}
+EOF
+
+echo "======== Test"
 ./gradlew ciTest --info --console=plain -PautomatedTests=true -PbuildNumber="${BUILD_NUMBER}"
 
 echo "======== Publish reports"
