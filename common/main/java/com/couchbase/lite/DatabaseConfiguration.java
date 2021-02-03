6f5773223e9d24303b4de6b6b667423a1fc77b91
G. Blake Meike <blake.meike@couchbase.com>
2021-02-03 14:21:23 -0800

    Fix scratch/database directory handling

--- common/main/java/com/couchbase/lite/DatabaseConfiguration.java
+++ common/main/java/com/couchbase/lite/DatabaseConfiguration.java
@@ -30,31 +30,12 @@ public final class DatabaseConfiguration extends AbstractDatabaseConfiguration {
     // Constructors
     //---------------------------------------------
 
-    @SuppressWarnings("ConstantConditions")
     @SuppressFBWarnings("NP_NULL_PARAM_DEREF_NONVIRTUAL")
     public DatabaseConfiguration() { this(null, false); }
 
     public DatabaseConfiguration(@NonNull DatabaseConfiguration config) { this(config, false); }
 
     DatabaseConfiguration(@Nullable DatabaseConfiguration config, boolean readOnly) { super(config, readOnly); }
-
-    //---------------------------------------------
-    // API - public methods
-    //---------------------------------------------
-
-    /**
-     * Set the path to the directory to store the database in. If the directory doesn't already
-     * exist it will be created when the database is opened.
-     *
-     * @param directory the directory
-     * @return The self object.
-     */
-    @NonNull
-    @Override
-    public DatabaseConfiguration setDirectory(@NonNull String directory) {
-        super.setDirectory(directory);
-        return this;
-    }
 
     //---------------------------------------------
     // protected methods
