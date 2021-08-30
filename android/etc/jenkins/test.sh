359d52ad640c885fedee1170cc2ac249eb9ea108
G. Blake Meike <blake.meike@couchbase.com>
2021-08-30 13:50:35 -0700

    Update location of sdkmanager

--- android/etc/jenkins/test.sh
+++ android/etc/jenkins/test.sh
@@ -33,15 +33,15 @@ if [ -z "REPORTS" ]; then
     usage
 fi
 
-SDK_MGR="${SDK_HOME}/tools/bin/sdkmanager"
+SDK_MGR="${SDK_HOME}/cmdline-tools/latest/bin/sdkmanager --channel=1 --install"
 
 echo "======== TEST Couchbase Lite Android, Community Edition v`cat ../../version.txt`-${BUILD_NUMBER} on device: ${ANDROID_SERIAL}"
 
 echo "======== Install Toolchain"
 yes | ${SDK_MGR} --licenses > /dev/null 2>&1
-${SDK_MGR} --install "build-tools;${BUILD_TOOLS_VERSION}"
-${SDK_MGR} --install "cmake;${CMAKE_VERSION}"
-${SDK_MGR} --install "ndk;${NDK_VERSION}"
+${SDK_MGR} "build-tools;${BUILD_TOOLS_VERSION}"
+${SDK_MGR} "cmake;${CMAKE_VERSION}"
+${SDK_MGR} "ndk;${NDK_VERSION}"
 
 cat <<EOF >> local.properties
 sdk.dir=${SDK_HOME}
