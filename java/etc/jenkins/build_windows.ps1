ece019d2340a14cfa34b0ecb2743e71d6cddb2ff
LaurenNguyen14 <lauren.nguyen@couchbase.com>
2022-03-02 12:09:32 -0800

    Stop gradle daemon after a gradle task is done

--- java/etc/jenkins/build_windows.ps1
+++ java/etc/jenkins/build_windows.ps1
@@ -21,7 +21,7 @@ Write-Host "======== Download Lite Core"
 & $toolsDir/fetch_litecore.ps1 $LiteCoreRepoUrl -Edition "CE"
 
 Write-Host "======== Build Java"
-$process = Start-Process -FilePath "$PSScriptRoot\..\..\gradlew.bat" -ArgumentList "ciBuild --info -PbuildNumber=$buildNumber" -PassThru -Wait
+$process = Start-Process -FilePath "$PSScriptRoot\..\..\gradlew.bat" -ArgumentList "--no-daemon ciBuild -PbuildNumber=$buildNumber" -PassThru -Wait
 if($process.ExitCode -ne 0){
     Write-Host "Failed with error" $process.ExitCode 
     exit $process.ExitCode
