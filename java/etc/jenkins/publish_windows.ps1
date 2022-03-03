8b20e7384bf3b1b05cccb24fa8eee39a96fa3aa5
LaurenNguyen14 <lauren.nguyen@couchbase.com>
2022-03-02 16:13:04 -0800

    fix windows publish script

--- java/etc/jenkins/publish_windows.ps1
+++ java/etc/jenkins/publish_windows.ps1
@@ -17,13 +17,13 @@ $mavenUrl= "http://proget.build.couchbase.com/maven2/cimaven"
 $status = 0
 
 Write-Host "======== PUBLISH Couchbase Lite Java for Windows, Community Edition"
-$process = Start-Process -FilePath "$PSScriptRoot\..\..\gradlew.bat" -ArgumentList "--no-daemon ciPublish -PbuildNumber=$buildNumber" -PassThru -Wait
+$process = Start-Process -FilePath "$PSScriptRoot\..\..\gradlew.bat" -ArgumentList "--no-daemon ciPublish -PbuildNumber=$buildNumber -PmavenUrl=$mavenUrl" -PassThru -Wait
 if($process.ExitCode -ne 0){
     $status = 5
 }
 
 Write-Host "======== Copy artifacts to staging directory"
-Copy-Item "lib\build\distributions\$product-$version-$buildNumber.zip $artifactsDir\$product-$version-$buildNumber-windows.zip"
+Copy-Item "lib\build\distributions\$product-$version-$buildNumber.zip" -Destination "$artifactsDir\$product-$version-$buildNumber-windows.zip"
 
 Write-Host "======== PUBLICATION COMPLETE $status"
 exit $status
