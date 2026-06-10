8d6719914d18e82553c8bee9cbbf8041a26f8e16
Jim Borden <jim.borden@couchbase.com>
2026-06-11 08:16:13 +0900

    Add back 3.x API for 3.4 release (#115)

--- common/main/java/com/couchbase/lite/Defaults.java
+++ common/main/java/com/couchbase/lite/Defaults.java
@@ -114,6 +114,8 @@ public final class Defaults {
          * Max wait time between retry attempts in seconds
          */
         public static final int MAX_ATTEMPTS_WAIT_TIME = 300;
+        @Deprecated
+        public static final int MAX_ATTEMPT_WAIT_TIME = MAX_ATTEMPTS_WAIT_TIME;
 
         /**
          * Purge documents when a user loses access
